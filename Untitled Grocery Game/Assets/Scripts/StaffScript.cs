2748c3deb664c1aaa00a88710cfc051739584158
CadenMcAllister <126972887+CadenMcAllister@users.noreply.github.com>
2023-08-08 19:11:54 -0500

    Staff Points to Mouse

--- Untitled Grocery Game/Assets/Scripts/StaffScript.cs
+++ Untitled Grocery Game/Assets/Scripts/StaffScript.cs
@@ -5,19 +5,20 @@ using UnityEngine;
 public class StaffScript : MonoBehaviour
 {
     public Transform spriteTransform;
+    public Camera mainCamera;
+
+    public Vector3 offset;
     // Start is called before the first frame update
     void Start()
     {
     spriteTransform = GetComponent<Transform>();
     }
 
-    // Update is called once per frame
-void Update()
-{
-    Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    void FixedUpdate(){
+    Vector3 mousePos = mainCamera.ScreenToWorldPoint(Input.mousePosition);
     mousePos.z = 0f;
     Vector3 direction = mousePos - spriteTransform.position;
     float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
     spriteTransform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
-}
+    }
 }
