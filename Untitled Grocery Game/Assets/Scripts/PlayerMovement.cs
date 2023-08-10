4138be06f4c5e389adba71e3d29650faca4c4a02
CadenMcAllister <126972887+CadenMcAllister@users.noreply.github.com>
2023-08-10 16:13:09 -0500

    Background and collision added to parts of the background

--- Untitled Grocery Game/Assets/Scripts/PlayerMovement.cs
+++ Untitled Grocery Game/Assets/Scripts/PlayerMovement.cs
@@ -13,7 +13,7 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start(){
 
-        Player  = GameObject.FindGameObjectWithTag("Player");
+        Player = GameObject.FindGameObjectWithTag("Player");
         rb = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody2D>();
         playerSprite = GameObject.FindGameObjectWithTag("Player").GetComponent<SpriteRenderer>();
     }
