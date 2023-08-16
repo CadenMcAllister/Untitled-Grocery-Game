01dc5f336988d0e99ebb79ab519f1f2c82742c86
CadenMcAllister <126972887+CadenMcAllister@users.noreply.github.com>
2023-08-16 16:20:23 -0500

    Enemies have health bars

---
+++ Untitled Grocery Game/Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{   
+    
+    public int maxHealth = 100;
+    public int currentHealth;
+
+    public HealthBar healthBar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+
+        healthBar.SetMaxHealth(maxHealth);
+    }
+    void TakeDamage(int damage){
+        currentHealth -= damage;
+
+        healthBar.SetHealth(currentHealth);
+    }
+
+    void Update(){
+        if (currentHealth == 0){
+            Destroy(gameObject);
+        }
+    }
+    
+    private void OnCollisionEnter2D(Collision2D collision){
+        if (collision.gameObject.CompareTag("Projectile")){
+            TakeDamage(20);
+        }
+    }
+}
