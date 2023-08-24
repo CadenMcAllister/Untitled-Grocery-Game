e2b55fbddc46a7023e4459c30d4ee8d0952843f3
CadenMcAllister <126972887+CadenMcAllister@users.noreply.github.com>
2023-08-24 05:41:51 -0500

    Spawning Enemies every 6 seconds

--- Untitled Grocery Game/Assets/Scripts/SpawnManager.cs
+++ Untitled Grocery Game/Assets/Scripts/SpawnManager.cs
@@ -4,9 +4,22 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    public GameObject[] Enemies;
+    private float timer;
     // Start is called before the first frame update
-    void Start()
+    void Update()
     {
+        timer += Time.deltaTime;
+
+        if (timer > 10){
+            timer = 0;
+            Spawn();
+        }
+    }
+
+    public void Spawn(){
+         int enemyIndex = Random.Range(0, Enemies.Length);
+        Instantiate(Enemies[enemyIndex], transform.position, Quaternion.identity);
     }
 
 
