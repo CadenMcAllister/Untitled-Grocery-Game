baefc4e81c2ac73c89bf300852372db087b366e1
CadenMcAllister <126972887+CadenMcAllister@users.noreply.github.com>
2023-08-25 16:39:06 -0500

    Mobile

--- Untitled Grocery Game/Assets/Scripts/MobileMovement.cs
+++ Untitled Grocery Game/Assets/Scripts/MobileMovement.cs
@@ -8,20 +8,16 @@ public class MobileMovement : MonoBehaviour
     public Rigidbody2D rb;
     public float speed = 7f;
     public Joystick joystick;
+
     // Start is called before the first frame update
     void Start()
     {
         Player = GameObject.FindGameObjectWithTag("Player");
         rb = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody2D>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
 
     }
 
-        void FixedUpdate(){
-        Vector2 direction = Vector2.up * joystick.Vertical + Vector3.right * joystick.Horizontal;
-        rb.AddForce(direction * speed * Time.fixedDeltaTime);    }
+        public void FixedUpdate(){
+        rb.AddForce (new Vector2(joystick.Horizontal * speed * Time.fixedDeltaTime, joystick.Vertical * speed * Time.fixedDeltaTime ), ForceMode2D.Impulse);
+        }
 }
