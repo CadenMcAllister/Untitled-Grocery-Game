b0fd2e58b38f80c4bdf10ad05fe67ff604c42aa7
CadenMcAllister <126972887+CadenMcAllister@users.noreply.github.com>
2023-08-29 15:38:07 -0500

    Small script change

--- Untitled Grocery Game/Assets/Scripts/BoltParticle.cs
+++ Untitled Grocery Game/Assets/Scripts/BoltParticle.cs
@@ -9,7 +9,9 @@ public class BoltParticle : MonoBehaviour
     public AudioSource Source;
     public AudioClip Clip;
     private void OnCollisionEnter2D(Collision2D collision){
+        if(!collision.gameObject.CompareTag("Player")){
         StartCoroutine(Collision());
+        }
     }
 
     IEnumerator Collision(){
@@ -17,6 +19,7 @@ public class BoltParticle : MonoBehaviour
         Instantiate(ExplosionObject, transform.position, transform.rotation);
         Source.PlayOneShot(Clip);
         yield return new WaitForSeconds(0.5f);
+        yield return new WaitForSeconds(0.1f);
         Destroy(gameObject);
     }
 }
